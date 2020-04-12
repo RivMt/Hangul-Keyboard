45e5d65e488874da0c4c413327d7468c6d1f9193
rivmt@ATIV <devrivmt@gmail.com>
2020-04-12 11:43:42 +0900

    Test

--- app/src/main/java/com/rivmt/kbd/hangulkeyboard/ime/IMEMaster.java
+++ app/src/main/java/com/rivmt/kbd/hangulkeyboard/ime/IMEMaster.java
@@ -5,6 +5,9 @@ public abstract class IMEMaster {
     public IMEInfoHandler mInfo = new IMEInfoHandler();
     public char mBlankWord = '0';
 
+    /*
+        This method check the keycode that controlled by IME
+     */
     public abstract boolean checkCode(int i);
 
     public abstract void createLetter(int i, boolean cont);
